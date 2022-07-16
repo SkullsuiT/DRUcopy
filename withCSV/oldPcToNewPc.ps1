865de6fec7e28bf4588025a3e2dab556ab344cc5
SkullsuiT <98453442+SkullsuiT@users.noreply.github.com>
2022-07-16 22:20:42 +0200

    Add files via upload

---
+++ withCSV/oldPcToNewPc.ps1
@@ -0,0 +1,21 @@
+$location = Get-Location # Le .CSV doit être dans le même dossier que le .PS1 / le cas échéant, commenter et indiquer le chemin
+$CSV = Import-Csv -Path $location -Encoding 'UTF8' -Delimiter ';'
+
+$log = "robocopy_$env:UserName.log"
+
+$Options = "*.* /tee /eta /MIR /MT:4 /j /R:5 /W:2 /XO /log+:$env:USERPROFILE\$log"
+
+# Lecture du .CSV
+foreach ($Line in $CSV) {
+        if(Test-Path -path "$($Line.Destination)" -ne $null) # Si le dossier de destination éxiste -> copie
+        {
+            $cmd = "Robocopy.exe '$($Line.Source)' '$($Line.Destination)' $Options"
+            Invoke-Expression $cmd
+        }
+        else
+        {
+            New-Item -ItemType "Directory" -Path $($Line.Destination) -Force # Sinon, création de celui-ci + copie
+            $cmd = "Robocopy.exe '$($Line.Source)' '$($Line.Destination)' $Options"
+            Invoke-Expression $cmd
+        }
+    }
