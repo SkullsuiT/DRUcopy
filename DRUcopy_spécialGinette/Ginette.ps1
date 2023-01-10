4715bb96e5fb05214e195b89eb6d15f7294a891a
unknown <jda-silva3@int.ac-nancy-metz.fr>
2023-01-10 13:56:24 +0100

    Update Ginette.ps1

--- DRUcopy_spécialGinette/Ginette.ps1
+++ DRUcopy_spécialGinette/Ginette.ps1
@@ -1,59 +1,68 @@
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
-# Mise en page
+<# Mise en page #>
 
-$Form                            = New-Object system.Windows.Forms.Form
-$Form.ClientSize                 = New-Object System.Drawing.Point(392,400)
-$Form.text                       = "DRUcopy"
-$Form.StartPosition              = [System.Windows.Forms.FormStartPosition]::CenterScreen
-$Form.TopMost                    = $true
+# Convertir une image en Base64 -> [Convert]::ToBase64String((Get-Content "PATH" -Encoding Byte))
+# Cette chaîne base64 contient les octets qui composent l'icône Marianne en 32x32 pixels
+    $iconBase64 = '/9j/4AAQSkZJRgABAQAAAQABAAD/4gKgSUNDX1BST0ZJTEUAAQEAAAKQbGNtcwQwAABtbnRyUkdCIFhZWiAAAAAAAAAAAAAAAABhY3NwQVBQTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA9tYAAQAAAADTLWxjbXMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAtkZXNjAAABCAAAADhjcHJ0AAABQAAAAE53dHB0AAABkAAAABRjaGFkAAABpAAAACxyWFlaAAAB0AAAABRiWFlaAAAB5AAAABRnWFlaAAAB+AAAABRyVFJDAAACDAAAACBnVFJDAAACLAAAACBiVFJDAAACTAAAACBjaHJtAAACbAAAACRtbHVjAAAAAAAAAAEAAAAMZW5VUwAAABwAAAAcAHMAUgBHAEIAIABiAHUAaQBsAHQALQBpAG4AAG1sdWMAAAAAAAAAAQAAAAxlblVTAAAAMgAAABwATgBvACAAYwBvAHAAeQByAGkAZwBoAHQALAAgAHUAcwBlACAAZgByAGUAZQBsAHkAAAAAWFlaIAAAAAAAAPbWAAEAAAAA0y1zZjMyAAAAAAABDEoAAAXj///zKgAAB5sAAP2H///7ov///aMAAAPYAADAlFhZWiAAAAAAAABvlAAAOO4AAAOQWFlaIAAAAAAAACSdAAAPgwAAtr5YWVogAAAAAAAAYqUAALeQAAAY3nBhcmEAAAAAAAMAAAACZmYAAPKnAAANWQAAE9AAAApbcGFyYQAAAAAAAwAAAAJmZgAA8qcAAA1ZAAAT0AAACltwYXJhAAAAAAADAAAAAmZmAADypwAADVkAABPQAAAKW2Nocm0AAAAAAAMAAAAAo9cAAFR7AABMzQAAmZoAACZmAAAPXP/bAEMABQMEBAQDBQQEBAUFBQYHDAgHBwcHDwsLCQwRDxISEQ8RERMWHBcTFBoVEREYIRgaHR0fHx8TFyIkIh4kHB4fHv/bAEMBBQUFBwYHDggIDh4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHv/CABEIAEkASQMBIgACEQEDEQH/xAAcAAACAwADAQAAAAAAAAAAAAAABwQFBgEDCAL/xAAbAQABBQEBAAAAAAAAAAAAAAAFAQIDBAYAB//aAAwDAQACEAMQAAABcvSedLjd3r84ynoAUH8dHyn4SbqIkuUaoVNa0ewreveaK9yU4A1RPtBfQGIjc84O+Mgtbu3WWnyTc1OfvxEnIESmd0C5iILOT0uKhuKGkbit1PmfS1ybTcYbc5ekQS5awxnoeictZZksAASjQDuAO4iSxFAFT//EACIQAAICAwABBAMAAAAAAAAAAAMEAQUAAgYQERUgMBIiI//aAAgBAQABBQLDFGET3UlOxU17OnmcXOJgfg5RhD0V0a0Y4umhUHloUmDqRmjsE2BtL539jOLzpB49PT4deCB2XJOyFzL8u57nKBnVuo8v6k2X6dHcSYd5EX3UGbOIpk1rubtlaJavBPw6FmFq7PzLlxfIq4GtLeO04VkRRPr5iYnO2J+ums77+yjzp6VUzorqpVCm8/cGSVEmtnWukXX0JvpJCbk25VOWLDHVhOKpcd/dJRdMHi9qfcYerHU8SVM4xWpjRU+hdYC8/Z//xAAlEQACAQIFAwUAAAAAAAAAAAABAgMABAUQERMxEiFRFCAiMkH/2gAIAQMBAT8BZtKHk5WNgLxWCt8xwKZSp6TTHU54DLt3i9+e1Y/YMZhJGOchlhcG9dohq/v4rTp3P2iDr2oCsNeFLlWm+tLNCy7oI081it76ycuOBx7AxA0y/8QAIhEAAgICAQQDAQAAAAAAAAAAAQIAAwQRMRASIWETIJEi/9oACAECAQE/AcTEbJb1Mp60Px0/vSyzsPqczEpFdIWOpVip6ZA2kx7PGjKwXXvO0978fkuO3J6WtpCZXWX4mNfU1er24mReLDpBoS0Er/M0eJVX2L9v/8QANBAAAgECAwUFBwMFAAAAAAAAAQIDABEEEjEFEBMhIiBBUWFxFBUjUoGhwTAzkWKC0eHw/9oACAEBAAY/AqaWV1RF1JoYTY8HEdjYOw19BQn2jinxGI8L9Ceg7HEicMu9pZWCoguSasLphlPQn5NDaE4+NKvSPlXsFFleI9zKdKyiVZPnUHX/AHSzxG6tuTZsbf1y/gVGZBdMwzelctOyJRpKl/rXsrHol08m3YqR9eIf43YaZWucgDeo17B4UaO/cGrDSLzSO4fyJpZF1U3FaiuFtHYZh9EU/fv/AJoyYUrARqVOUj1BqX3fM0g5LJ1XUt/nssWh4ofotfd308M2GlmscpGUZSfrXFwWBGCwx1a/T/3pQ2dhznMYvIR4+fn2ORvWHi6ed286CjUm273jicUMNBl+LyuSfKlw8T7SliUWAzZR9qGC2XH7BhE/cZdbevjSwQiyr49+5MPE2Uy3zEeFXR2U+RrNI7O3ixvQmI+HD1fXu3Ph51zI4saY4zE3iB6RHqw/FCHDRLGg8N6MsvDkTS45GrzRHL868xQhhW7H7UsEf9x8T+ixhiSPObmw/V//xAAlEAEAAQMDAwUBAQAAAAAAAAABEQAhUTFBYRCBkSChscHwcTD/2gAIAQEAAT8hosbS+AKzM28/F3xQK/vp8AWXnqkJCeKIqTEmzhw9UWs04CmSay9fs+KQRrPe+349CyaaMqrjfe3CdHG7vUPtTyO489AWoQg3/E+KgYDDMr1a2WWj0Ni9AuQlGFn6pxz/APi+nipoRURA7Cx7B07LCoo9BxLFjJ4zRQoOQJn+Talug/8AYZ6BA/DDCPMr0ewjWXYU8h1RGN8WJMenV9MoBI3enL41fYFmYF31SK2hk9mXiie4UrT9ixxQaEYt10+hhqCOpTbYg8X9qIeRD+tcJSgtIiK2O4+Ks/1DF4NG8vrsLOTzzTT8pKt1c9HMZYR27zXJYEGm7Bqhe9brJOdr77dLcxjHJzUc0GFGwlfhWwmTc5Xd6lEBS4BzTql+i7d63h0dhl4q/BF93ef8EEhJGklmg0v+v//aAAwDAQACAAMAAAAQ4N8q3ed8BAt+8+CTXt/M888f8//EACERAQABAwUBAAMAAAAAAAAAAAERACExEEFRcYGRIGHR/9oACAEDAQE/EANFdCO1c5C83wbQ85zNPxhLJw1cKGSdIEwA9rSH0KmOxvHJF/RPlMDGaw0ADaZcmBdodqWAm6PI/tGkKizmsTpnEkwxPsUleBgkR3X6UXQ39b/D8FhUHJs96f/EACIRAQABBAEEAwEAAAAAAAAAAAERACExQVEQYXGBILHB8P/aAAgBAgEBPxCMFhl/DvTooCzsu78eM+Oikp5UIJKF+yXy5rKgMdJ3tRTbFSThvkXszf3TTCmlymn+10iipLhUrnEOO0Ez7xVqR1teV2/VNTKk2ZfirQ538EFnp//EACYQAQACAgIBAwQDAQAAAAAAAAERIQAxQVFxEGGBIDCRsaHh8dH/2gAIAQEAAT8Qx2wy/fK4m2R0qc0wbZYAlhlGdYUrovcTwG30ZoiTDb4xAeEthSGw5G/VFySQNKuJ/BoUqvb60KOVYxbFghoxuMGeEHL9CLnFElN0nt+t5WbBazAD0AHZCdplf6w+EuA0+i7Ekuk3/hZkiyU6Ao/E57DHRHEfQhFAG1wBRiFKzfJOffGVbwqAqOoHkmIYoT27THgjBgSHNiEnDJPhHn6EkE/gpU9JrKrH0SNLFSD0KOcWUvhiAD9Z/rZH+EO4obP2McUKQ+SpMHsI6cgniMDNwnEKtk39JWXJhcSS0E4uY16uVrfQgnYJq6PC45ckbkWUMcFUDuN5okKqglcpZQqhwIUJZwqGYTZ6iLLIYmH4yFHKIaQR0peX2ZDGZ9pB/Ln+pijU6JKXLActKZcnyfqkmiUT8bycykLKJYD2AsyqBQu3Uq5a202vpOGoqBAIeJQ8Cc4CD7Dr5HAjxCp+UuIDYFFf9Cf7Mh7fzgTFeR0TgMI8JhkJZQRsSEmwPN430pjvmRadqvqD+xxgYhZCbJ3rDQnVv5H6DFEplPPfgf0XhLzvSF7f0HAB9h2YEIkiZLJBn50ce2re/u//2SAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIA=='
+    $iconBytes = [Convert]::FromBase64String($iconBase64)
 
-$Sauvegarde                      = New-Object system.Windows.Forms.RadioButton
-$Sauvegarde.text                 = "Sauvegarde"
-$Sauvegarde.AutoSize             = $true
-$Sauvegarde.width                = 200
-$Sauvegarde.height               = 20
-$Sauvegarde.location             = New-Object System.Drawing.Point(145,108)
-$Sauvegarde.Font                 = New-Object System.Drawing.Font('Marianne',11,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-$Sauvegarde.ForeColor            = [System.Drawing.ColorTranslator]::FromHtml("#00c7fc")
+# Initialise un flux de mémoire contenant les octets
+    $stream = [System.IO.MemoryStream]::new($iconBytes, 0, $iconBytes.Length)
 
-$RadioButton2                    = New-Object system.Windows.Forms.RadioButton
-$RadioButton2.text               = "Restauration"
-$RadioButton2.AutoSize           = $true
-$RadioButton2.width              = 200
-$RadioButton2.height             = 20
-$RadioButton2.location           = New-Object System.Drawing.Point(145,174)
-$RadioButton2.Font               = New-Object System.Drawing.Font('Marianne',11,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-$RadioButton2.ForeColor          = [System.Drawing.ColorTranslator]::FromHtml("#ff0000")
+    $Form                            = New-Object system.Windows.Forms.Form
+    $Form.ClientSize                 = New-Object System.Drawing.Point(392,400)
+    $formCHX.Icon = [System.Drawing.Icon]::FromHandle(([System.Drawing.Bitmap]::new($stream).GetHIcon()))
+    $Form.text                       = "DRUcopy"
+    $Form.StartPosition              = [System.Windows.Forms.FormStartPosition]::CenterScreen
+    $Form.TopMost                    = $true
 
-$OK                              = New-Object system.Windows.Forms.Button
-$OK.text                         = "OK !"
-$OK.width                        = 130
-$OK.height                       = 30
-$OK.location                     = New-Object System.Drawing.Point(15,327)
-$OK.Font                         = New-Object System.Drawing.Font('Marianne',11,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+    $Sauvegarde                      = New-Object system.Windows.Forms.RadioButton
+    $Sauvegarde.text                 = "Sauvegarde"
+    $Sauvegarde.AutoSize             = $true
+    $Sauvegarde.width                = 200
+    $Sauvegarde.height               = 20
+    $Sauvegarde.location             = New-Object System.Drawing.Point(145,108)
+    $Sauvegarde.Font                 = New-Object System.Drawing.Font('Marianne',11,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+    $Sauvegarde.ForeColor            = [System.Drawing.ColorTranslator]::FromHtml("#00c7fc")
 
-$ProgressBar1                    = New-Object system.Windows.Forms.ProgressBar
-$ProgressBar1.width              = 367
-$ProgressBar1.height             = 23
-$ProgressBar1.location           = New-Object System.Drawing.Point(14,367)
+    $RadioButton2                    = New-Object system.Windows.Forms.RadioButton
+    $RadioButton2.text               = "Restauration"
+    $RadioButton2.AutoSize           = $true
+    $RadioButton2.width              = 200
+    $RadioButton2.height             = 20
+    $RadioButton2.location           = New-Object System.Drawing.Point(145,174)
+    $RadioButton2.Font               = New-Object System.Drawing.Font('Marianne',11,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+    $RadioButton2.ForeColor          = [System.Drawing.ColorTranslator]::FromHtml("#ff0000")
 
-$Cancel                          = New-Object system.Windows.Forms.Button
-$Cancel.text                     = "STOPTOU"
-$Cancel.width                    = 130
-$Cancel.height                   = 30
-$Cancel.location                 = New-Object System.Drawing.Point(249,327)
-$Cancel.Font                     = New-Object System.Drawing.Font('Marianne',11,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+    $OK                              = New-Object system.Windows.Forms.Button
+    $OK.text                         = "OK !"
+    $OK.width                        = 130
+    $OK.height                       = 30
+    $OK.location                     = New-Object System.Drawing.Point(15,327)
+    $OK.Font                         = New-Object System.Drawing.Font('Marianne',11,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
-$Form.controls.AddRange(@($Sauvegarde,$RadioButton2,$OK,$ProgressBar1,$Cancel))
+    $ProgressBar1                    = New-Object system.Windows.Forms.ProgressBar
+    $ProgressBar1.width              = 367
+    $ProgressBar1.height             = 23
+    $ProgressBar1.location           = New-Object System.Drawing.Point(14,367)
 
-# Fin mise en page
+    $Cancel                          = New-Object system.Windows.Forms.Button
+    $Cancel.text                     = "STOPTOU"
+    $Cancel.width                    = 130
+    $Cancel.height                   = 30
+    $Cancel.location                 = New-Object System.Drawing.Point(249,327)
+    $Cancel.Font                     = New-Object System.Drawing.Font('Marianne',11,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
-# Logic 
+    $Form.controls.AddRange(@($Sauvegarde,$RadioButton2,$OK,$ProgressBar1,$Cancel))
+
+<# Fin Mise en page #>
+
+<# Logic #>
 
 
 
-# Fin Logic
+<# Fin Logic #>
 
-[void]$Form.ShowDialog()
+    [void]$Form.ShowDialog()
